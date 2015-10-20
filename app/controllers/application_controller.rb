432cb33d6cadebf594fe457ffce8d62cae89ded4
bilalbash <bilal.basharat@gmail.com>
2015-10-21 02:19:11 +0500

    download agent rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
 
   def welcome
   end
+
+  def download_agent
+    file_name = 'agent.rb'
+    f = File.open(file_name, 'w')
+    f.write("require 'usagewatch_ext'")
+    f.write("\n")
+    f.write("usw =  Usagewatch")
+    f.write("\n")
+    f.write('puts "#{usw.uw_diskused} Gigabytes Used"')
+    f.write("\n")
+    f.close
+    send_file f.path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#welcome'
 
+  get '/download/agent', to: 'application#download_agent', as: 'download_agent'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
